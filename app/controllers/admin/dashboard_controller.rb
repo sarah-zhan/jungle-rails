7a75c996aeb17f8a77f395e92bc5df67b99b74eb
sarah-zhan <yizh.contact@gmail.com>
2023-04-05 17:02:51 -0700

    add admin dash board, change button color to green

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class Admin::DashboardController < ApplicationController
+  # fetch the data and pass it to the template
+  # get '/products/:id', to: 'products'
+  # get '/categories/:id', to: 'categories'
+  # num_product = Product.count
+  # num_category = Category.count
+  # set instance variables in the controller action with values, and then render those instance variables in the ERB template
+
+  # Display a count of how many products are in the database
+  # Display a count of how many categories are in the database
+
   def show
+    @num_product = Product.all.count
+    @num_category = Category.all.count
   end
 end
