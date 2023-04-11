4dffa44c281b1f36703d16758d7778cf1952ef12
sarah-zhan <yizh.contact@gmail.com>
2023-04-10 23:07:06 -0700

    add rspec for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@
 # last_name :string
 # email :string
 # password_digest :string
-# 
-#password:string virtual
-#password_confirmation:string virtual
-
+#
+# password:string virtual
+# password_confirmation:string virtual
+# create user class
 class User < ApplicationRecord
   has_secure_password
-
-  validates :email, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}, presence: true
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' }, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password_digest, presence: true, length: { minimum: 8, message: 'must be at least 8 characters' }
 end
---
+++ spec/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it 'returns false password and password_confirmation fields not match' do
+      user = User.new(password: 'password', password_confirmation: 'password2')
+      expect(user.valid?).to eq(false)
+    end
+    xit 'returns true password and password_confirmation fields match' do
+      user = User.new(password: 'password2', password_confirmation: 'password2')
+      expect(user.valid?).to eq(true)
+    end
+    it 'Emails must be unique' do
+      user1 = User.create(email: '123@gmail.com')
+      user2 = User.new(email: '123@gmail.com')
+      expect(user2.valid?).to eq(false)
+    end
+    it 'should require email' do
+      user = User.new(email: nil)
+      expect(user).not_to be_valid
+    end
+    it 'should require first name' do
+      user = User.new(first_name: nil)
+      expect(user).not_to be_valid
+    end
+    it 'should require last name' do
+      user = User.new(last_name: nil)
+      expect(user).not_to be_valid
+    end
+    it 'must have a minimum length of 8 for password' do
+      user = User.new(password: '123', password_confirmation: '123')
+      expect(user.valid?).to eq(false)
+    end
+  end
+end
