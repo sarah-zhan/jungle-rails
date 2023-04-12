bc9d05626f89f421b806933fb1f3fc0a45dfbf19
sarah-zhan <yizh.contact@gmail.com>
2023-04-11 21:13:36 -0700

    add test for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+
+describe('Navigation', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it('There are 2 products on the page', () => {
+    cy.get('.products article').should('have.length', 2);
+  });
+
+});
